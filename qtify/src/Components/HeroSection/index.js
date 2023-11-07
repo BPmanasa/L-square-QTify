cdd46829dbdf7bf506b6369bcabab63af47c3a99
manasabp <manasabpmanasabp77@gmail.com>
2023-11-07 17:29:29 +0530

    hero-qtify-takehome

---
+++ qtify/src/Components/HeroSection/index.js
@@ -0,0 +1,13 @@
+import "./HeroSection.css";
+export default () => {
+  return (
+    <div className="background">
+      <div className="bg">
+        <h1 className="content">
+          100 Thousand Songs, add-free Over thousands podcast episodes
+        </h1>
+        <img className="headphone" src="Headphone.png" />
+      </div>
+    </div>
+  );
+};
