168f23a855f2e422142f89e99e1f6cb47186fc04
manasabp <manasabpmanasabp77@gmail.com>
2023-11-07 20:46:12 +0530

    final commit

--- qtify/src/Components/HeroSection/index.js
+++ qtify/src/Components/HeroSection/index.js
@@ -3,7 +3,7 @@ export default () => {
   return (
     <div className="background">
       <div className="bg">
-        <h1 className="content">
+        <h1 className="content ">
           100 Thousand Songs, add-free Over thousands podcast episodes
         </h1>
         <img className="headphone" src="Headphone.png" />
