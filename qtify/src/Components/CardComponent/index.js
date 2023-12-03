d548ebbf6619ac139c5ca32ce71d7692c8689e3f
manasabp <manasabpmanasabp77@gmail.com>
2023-12-03 12:45:34 +0530

    tooltip

--- qtify/src/Components/CardComponent/index.js
+++ qtify/src/Components/CardComponent/index.js
@@ -1,19 +1,27 @@
 import "./CardComponent.css";
-const CardComponent = ({ imgSrc, label, followersCount }) => {
+import Tooltip from "@mui/material/Tooltip";
+
+const CardComponent = ({ imgSrc, label, followersCount, numOfSongs }) => {
   return (
-    <div className="card-wrapper">
-      <div className="card">
-        <div className="card-img-frame">
-          <img className="card-img" src={imgSrc} alt="card" />
-        </div>
-        <div className="card-content">
-          <div className="card-content-pill">
-            <p>{followersCount} Follows</p>
+    <Tooltip
+      title={numOfSongs ? `${numOfSongs} songs` : ""}
+      placement="top"
+      arrow
+    >
+      <div className="card-wrapper">
+        <div className="card">
+          <div className="card-img-frame">
+            <img className="card-img" src={imgSrc} alt="card" />
+          </div>
+          <div className="card-content">
+            <div className="card-content-pill">
+              <p>{followersCount} Follows</p>
+            </div>
           </div>
         </div>
+        <p className="card-label">{label}</p>
       </div>
-      <p className="card-label">{label}</p>
-    </div>
+    </Tooltip>
   );
 };
 
